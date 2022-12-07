d9988eeb186bd731858ee78d4a5399424cafbdf8
karlaclavel <karla.clavel1@gmail.com>
2022-12-06 21:58:57 -0600

    Metodo implementado en el boton Agregar para que habra el menu principal si el usuario y contraseña es correcto

--- src/main/java/mx/uam/ayd/proyecto/presentacion/IniciarSesion/ControlIniciarSesion.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/IniciarSesion/ControlIniciarSesion.java
@@ -3,7 +3,10 @@
  */
 package mx.uam.ayd.proyecto.presentacion.IniciarSesion;
 
+import mx.uam.ayd.proyecto.presentacion.principal.ControlPrincipal;
+
 import org.springframework.stereotype.Component;
+
 
 /**
  * Modulo de control para el inicio de sesión
@@ -15,4 +18,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class ControlIniciarSesion {
 
+	private ControlPrincipal control;
+	
+	private VentanaIniciarSesion is;
+	
+	String usuario = "PANROSA-ADMIN";
+	String contraseña = "BVEKUAMIS22";
+	
+	
+	public void ValidarDatos(String user, String pass) {
+		// TODO Auto-generated method stub
+		if (user.equals(usuario)) {
+			is.dispose();
+		}
+		
+	}
+	
+	
 }
--- src/main/java/mx/uam/ayd/proyecto/presentacion/IniciarSesion/VentanaIniciarSesion.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/IniciarSesion/VentanaIniciarSesion.java
@@ -7,13 +7,19 @@ import javax.swing.border.EmptyBorder;
 import org.springframework.stereotype.Component;
 
 import mx.uam.ayd.proyecto.presentacion.agregarCliente.ControlAgregarCliente;
+import mx.uam.ayd.proyecto.presentacion.principal.ControlPrincipal;
 
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 
@@ -22,19 +28,26 @@ import javax.swing.SwingConstants;
 @Component
 public class VentanaIniciarSesion extends JFrame {
 	
+
 	private JPanel contentPane;
 	
 	private JTextField textFieldUsuario;
 	private JTextField textFieldContraseña;
 	
 	private ControlIniciarSesion controlsesion;
+	private ControlPrincipal control;
 	
 	
 	public VentanaIniciarSesion() {
 		
+		JFrame ventana = new JFrame ("Ventana");
+		ventana.setSize(300, 400);
+		ventana.setResizable(false);
+		ventana.setVisible(true);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Iniciar Sesión");
-		setBounds(100, 100, 339, 442);
+		setBounds(100, 100, 330, 400);
 		
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(237, 216, 218));
@@ -44,30 +57,30 @@ public class VentanaIniciarSesion extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("Inicio de Sesión");
+		lblNewLabel_1.setBounds(71, 181, 172, 13);
 		lblNewLabel_1.setFont(new Font("Courier New", Font.BOLD, 15));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(71, 181, 172, 13);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Usuario:");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblNewLabel_2.setBounds(50, 212, 76, 13);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Contraseña:");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblNewLabel_3.setBounds(50, 246, 76, 13);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		contentPane.add(lblNewLabel_3);
 		
 		JLabel lblNewLabelTitulo = new JLabel("Service TEC");
+		lblNewLabelTitulo.setBounds(71, 130, 172, 41);
 		lblNewLabelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabelTitulo.setFont(new Font("Typo Grotesk Black", Font.PLAIN, 28));
-		lblNewLabelTitulo.setBounds(71, 130, 172, 41);
 		contentPane.add(lblNewLabelTitulo);
 		
 		JLabel lblNewLabelLogo = new JLabel("");
-		lblNewLabelLogo.setIcon(new ImageIcon("D:\\Archivos\\Documentos\\SPRING TOOL SUITE\\workspace\\Service Tec\\src\\main\\java\\mx\\uam\\ayd\\imagen\\logo.png"));
-		lblNewLabelLogo.setBounds(88, 25, 119, 108);
+		lblNewLabelLogo.setBounds(90, 10, 125, 110);
+		lblNewLabelLogo.setIcon(new ImageIcon("\\Service Tec\\src\\main\\java\\mx\\uam\\ayd\\imagen\\logo.png"));
 		contentPane.add(lblNewLabelLogo);
 		
 		/******** CUADROS DE TEXTO ******/
@@ -88,6 +101,28 @@ public class VentanaIniciarSesion extends JFrame {
 		btnNewButtonAceptar.setBounds(122, 300, 85, 21);
 		contentPane.add(btnNewButtonAceptar);
 		
+		btnNewButtonAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String user = textFieldUsuario.getText();
+				String pass = textFieldContraseña.getText();
+				
+				if (user.equals("") || pass.equals("")) {
+					muestraDialogoConMensaje ("Acceso Denegado! \n Ingrese Usuario y Contraseña correctos");
+					textFieldUsuario.requestFocus();
+				}
+				else {
+					if (user.equals(controlsesion.usuario) && pass.equals(controlsesion.contraseña)) {
+						controlsesion.ValidarDatos(user, pass);
+						control.inicia();
+					}
+					textFieldUsuario.setText("");
+					textFieldContraseña.setText("");
+				}
+			}
+			
+		});
+		
 
 		
 	}
@@ -95,7 +130,10 @@ public class VentanaIniciarSesion extends JFrame {
 	public void muestra(ControlIniciarSesion control) {
 		
 		this.controlsesion = control;
-
+		
+		textFieldUsuario.setText("");
+		textFieldContraseña.setText("");
+		
 		setVisible(true);
 		
 	}
