ccf9d58debcf5043de0505ca5d7b03f4f78b1120
unknown <erikamaya840@gmail.com>
2022-12-17 12:58:46 -0600

    Realice los cambios en la Ventana Eliminar Personal según el estándar de diseño de interfaz de ususario

--- src/main/java/mx/uam/ayd/proyecto/presentacion/eliminarPersonal/VentanaEliminarPersonal.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/eliminarPersonal/VentanaEliminarPersonal.java
@@ -63,9 +63,9 @@ public class VentanaEliminarPersonal extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblTitulo = new JLabel("Eliminar Personal");
-		lblTitulo.setForeground(new Color(199, 21, 133));
-		lblTitulo.setFont(new Font("Sitka Subheading", Font.BOLD, 25));
-		lblTitulo.setBounds(131, 11, 244, 32);
+		lblTitulo.setForeground(new Color(158,0,82));
+		lblTitulo.setFont(new Font("Sitka Subheading", Font.BOLD, 30));
+		lblTitulo.setBounds(107, 11, 268, 32);
 		contentPane.add(lblTitulo);
 		
 		JSeparator separator = new JSeparator();
@@ -74,87 +74,87 @@ public class VentanaEliminarPersonal extends JFrame {
 		contentPane.add(separator);
 		
 		JLabel lblSelecPersonal = new JLabel("Seleccione al Personal que desea eliminar");
-		lblSelecPersonal.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		lblSelecPersonal.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblSelecPersonal.setBounds(13, 57, 244, 14);
 		contentPane.add(lblSelecPersonal);
 		
 		JComboBox comboBoxPersonal = new JComboBox();
-		comboBoxPersonal.setBounds(254, 54, 158, 22);
+		comboBoxPersonal.setBounds(254, 54, 121, 22);
 		contentPane.add(comboBoxPersonal);
 		
 		JRadioButton rdbtnVerInformacion = new JRadioButton("");
 		rdbtnVerInformacion.setBackground(new Color(255, 182, 193));
-		rdbtnVerInformacion.setBounds(418, 53, 30, 23);
+		rdbtnVerInformacion.setBounds(385, 53, 30, 23);
 		contentPane.add(rdbtnVerInformacion);
 		
 		JLabel lblAviso = new JLabel("¡Antes de eliminar, por favor verifique que los datos correspondan al personal seleccionado!");
-		lblAviso.setForeground(new Color(220, 20, 60));
+		lblAviso.setForeground(new Color(128, 0, 128));
 		lblAviso.setBackground(new Color(139, 0, 139));
-		lblAviso.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		lblAviso.setBounds(10, 79, 446, 14);
+		lblAviso.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblAviso.setBounds(13, 82, 446, 14);
 		contentPane.add(lblAviso);
 		
 		JLabel lblDatos = new JLabel("DATOS DEL PERSONAL SELECCIONADO ");
 		lblDatos.setForeground(new Color(128, 0, 128));
-		lblDatos.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblDatos.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		lblDatos.setBackground(new Color(139, 0, 139));
 		lblDatos.setBounds(10, 104, 244, 14);
 		contentPane.add(lblDatos);
 		
 		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblNombre.setBounds(20, 129, 61, 14);
 		contentPane.add(lblNombre);
 		
 		textField_NombreCompleto = new JTextField();
-		textField_NombreCompleto.setBounds(78, 127, 370, 20);
+		textField_NombreCompleto.setBounds(78, 127, 347, 20);
 		contentPane.add(textField_NombreCompleto);
 		textField_NombreCompleto.setColumns(10);
 		
 		JLabel lblCorreo = new JLabel("Correo");
-		lblCorreo.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		lblCorreo.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblCorreo.setBounds(20, 164, 61, 14);
 		contentPane.add(lblCorreo);
 		
 		textField_Correo = new JTextField();
 		textField_Correo.setColumns(10);
-		textField_Correo.setBounds(78, 162, 370, 20);
+		textField_Correo.setBounds(78, 162, 347, 20);
 		contentPane.add(textField_Correo);
 		
 		JLabel lblTelfono = new JLabel("Teléfono");
-		lblTelfono.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		lblTelfono.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblTelfono.setBounds(20, 205, 61, 14);
 		contentPane.add(lblTelfono);
 		
 		textField_Telefono = new JTextField();
 		textField_Telefono.setColumns(10);
-		textField_Telefono.setBounds(78, 203, 158, 20);
+		textField_Telefono.setBounds(78, 203, 148, 20);
 		contentPane.add(textField_Telefono);
 		
 		JLabel lblPuesto = new JLabel("Puesto");
-		lblPuesto.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-		lblPuesto.setBounds(245, 206, 61, 14);
+		lblPuesto.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPuesto.setBounds(236, 205, 61, 14);
 		contentPane.add(lblPuesto);
 		
 		textField_Puesto = new JTextField();
 		textField_Puesto.setColumns(10);
-		textField_Puesto.setBounds(290, 203, 158, 20);
+		textField_Puesto.setBounds(277, 203, 145, 20);
 		contentPane.add(textField_Puesto);
 		
 		JButton btnNewButton = new JButton("Eliminar ");
-		btnNewButton.setBackground(new Color(255, 192, 203));
-		btnNewButton.setFont(new Font("Sitka Subheading", Font.BOLD, 16));
-		btnNewButton.setBounds(107, 247, 105, 23);
+		btnNewButton.setBackground(new Color(255, 228, 225));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton.setBounds(137, 238, 100, 32);
 		contentPane.add(btnNewButton);
 		
 		JButton btnCnacelar = new JButton("Cancelar");
-		btnCnacelar.setBackground(new Color(255, 192, 203));
+		btnCnacelar.setBackground(new Color(255, 228, 225));
 		btnCnacelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnCnacelar.setFont(new Font("Sitka Subheading", Font.BOLD, 16));
-		btnCnacelar.setBounds(277, 247, 110, 23);
+		btnCnacelar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnCnacelar.setBounds(254, 238, 100, 32);
 		contentPane.add(btnCnacelar);
 	}
 	
